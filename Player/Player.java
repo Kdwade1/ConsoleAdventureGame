70945f8f97a07f9c184fd3b41b5377c9535851b5
Kelvin <kelvin.daquon.wade@gmail.com>
2023-03-10 13:30:47 -0600

    started game

---
+++ Player/Player.java
@@ -0,0 +1,13 @@
+package Player;
+
+abstract class Player {
+    public String name;
+    public int health;
+    int minDamage;
+    int maxDamage;
+    int missChances;
+    int criticalRate;
+    int speed;
+    int numberOfPotions;
+    int runChance;
+}
---
+++ Player/Tank.java
@@ -0,0 +1,20 @@
+package Player;
+
+public class Tank {
+
+    public String name;
+    public int health;
+    int minDamage;
+    int maxDamage;
+    int missChances;
+    int criticalRate;
+    int speed;
+    int numberOfPotions;
+    int runChance;
+
+
+    public Tank(String name, int health,int minDamage,int maxDamage,int missChances,int criticalRate,int speed,int numberOfPotions, int runChance){
+
+    }
+
+}
