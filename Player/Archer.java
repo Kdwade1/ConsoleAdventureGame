b2907167d2c3923c099790b6692a799babae41de
Kelvin Wade <kelvin.daquon.wade@gmail.com>
2024-12-05 04:00:48 -0600

    update the player class

--- Player/Archer.java
+++ Player/Archer.java
@@ -1,76 +1,13 @@
 package Player;
 
-public class Archer {
-    public String name;
-    public int health;
-    int maxHealth;
-    int minDamage;
-    int maxDamage;
-    int missChances;
-    int criticalRate;
-    int speed;
-    int numberOfPotions;
-    int runChance;
-    int armorSlot;
-    int weaponSlot;
-
-
-   private Archer(String name) {
-        this.name = name + " The Archer";
-        this.health = 75;
-        this.maxHealth = 75;
-        this.minDamage = 5;
-        this.maxDamage = 10;
-        this.missChances = 30;
-        this.criticalRate = 25;
-        this.speed = 20;
-        this.numberOfPotions = 3;
-        this.runChance = 40;
-        this.armorSlot = 1;
-        this.weaponSlot = 1;
+public class Archer extends Player{
+    public Archer(String name) {
+        super(name + " The Archer", 75, 75, 5, 10, 30,40, 30, 3, 65);
     }
 
-    private int calculateDamage() {
-        if (Math.random() * 100 < missChances) {
-            System.out.println("Your arrows missed!");
-            return 0;
-
-        }
-        int damage = (int) (Math.random() * (maxDamage - minDamage + 1)) + minDamage;
-        if (Math.random() * 100 < criticalRate) {
-            damage *= 2;
-            System.out.println("HeadShot!");
-
-
-        }
-
-
-        return damage;
-    }
-
-    public int attack() {
-        int damage = calculateDamage();
-        System.out.println(name + " dealt " + damage + " damage!");
-        return damage;
-    }
-
-    public void usePotion() {
-        if (numberOfPotions > 0) {
-            health += 20;
-            if (health > maxHealth) {
-                health = maxHealth;
-
-            }
-            numberOfPotions--;
-            System.out.println(name + " used a potion. Current health: " + health);
-        } else {
-            System.out.println(name + " has no potions left!");
-        }
+    @Override
+    public void specialAttack() {
+        System.out.println(name+ "fires a powerful shot");
+       int damage = calculateDamage() * 2;
     }
 }
-
-
-
-
-
-
--- Player/Player.java
+++ Player/Player.java
@@ -1,8 +1,9 @@
 package Player;
 
-abstract class Player {
+public abstract class Player {
     public String name;
     public int health;
+    int maxHealth;
     int minDamage;
     int maxDamage;
     int missChances;
@@ -10,4 +11,62 @@ abstract class Player {
     int speed;
     int numberOfPotions;
     int runChance;
+// constructor
+    public Player(String name, int health, int maxHealth, int minDamage, int maxDamage,
+                  int missChances, int criticalRate, int speed, int numberOfPotions, int runChance) {
+        this.name = name;
+        this.health = health;
+        this.maxHealth = maxHealth;
+        this.minDamage = minDamage;
+        this.maxDamage = maxDamage;
+        this.missChances = missChances;
+        this.criticalRate = criticalRate;
+        this.speed = speed;
+        this.numberOfPotions = numberOfPotions;
+        this.runChance = runChance;
+    }
+//calculate the actual damage
+ protected int calculateDamage() {
+        if (Math.random() * 100 < missChances) {
+            System.out.println("Your arrows missed!");
+            return 0;
+
+        }
+        int damage = (int) (Math.random() * (maxDamage - minDamage + 1)) + minDamage;
+        if (Math.random() * 100 < criticalRate) {
+            damage *= 2;
+            System.out.println("HeadShot!");
+
+
+        }
+
+return damage;
+    }
+    //calculates  attack
+    public int attack() {
+        int damage = calculateDamage();
+        System.out.println(name + " dealt " + damage + " damage!");
+        return damage;
+    }
+//use Potion
+    public void usePotion() {
+        if (numberOfPotions > 0) {
+            health += 20;
+            if (health > maxHealth) {
+                health = maxHealth;
+
+            }
+            numberOfPotions--;
+            System.out.println(name + " used a potion. Current health: " + health);
+        } else {
+            System.out.println(name + " has no potions left!");
+        }
+    }
+
+    public abstract void specialAttack();
 }
+
+
+
+
+
